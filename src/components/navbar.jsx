253bbfe434bcaed4ebbde4bfea52b353ba4d065f
tajwarsaiyeed <rjabid36@gmail.com>
2025-10-13 11:00:11 +0600

    Implement Navbar component and update App structure

---
+++ src/components/navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { navLinks } from "../contants";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <img src="/logo.svg" alt="apple-logo" />
+        <ul>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a href={`#${item.label.toLowerCase()}`}>{item.label}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex-center gap-3">
+          <button>
+            <img src="/search.svg" alt="search" />
+          </button>
+          <button>
+            <img src="/cart.svg" alt="cart" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
